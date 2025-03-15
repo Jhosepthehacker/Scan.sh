fd17770dac29c187243a2ef7968cc58a2e8eda3f
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-15 00:20:45 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Entrada de datos
+
+read -p "¿Eres root? Y/N: " root
+
+#Comprobando la respuesta del usuario y dando permisos de ejecución a Scan.sh
+
+if [[ "$root" == "Y" ]]; [[ "$root" == "y" ]]; then
+
+     sudo chmod +x Scan.sh
+           echo -e "\e[31m Ya se dieron correctamente los permisos de ejecución a Scan.sh"
+
+else
+           chmod +x Scan.sh
+                 echo -e "\e[31m Ya se dieron correctamente los permisos de ejecución a Scan.sh"
+
+fi
+
+#Entrada de datos
+
+echo ""
+
+read -p "¿instalar con apt o pkg?: " apt
+
+#Comprobando la respuesta del usuario
+
+if [[ "$apt" == "apt" ]]; then
+
+echo "Instalando nmap cómo root"
+          sudo apt install nmap
+else
+        pkg install nmap
+fi
